4950ce2f84eb9b94efb371a99dee2045584b28bc
Jason <70564653+BioxMech@users.noreply.github.com>
2020-10-20 23:29:11 +0800

    Update in form.js and index.html

---
+++ js/form.js
@@ -0,0 +1,13 @@
+function clearAllSignin() {
+    document.getElementById("signinEmail").value = '';
+    document.getElementById("signinPassword").value = '';
+}
+
+function clearAllRegister() {
+    document.getElementById("registerEmail").value = '';
+    document.getElementById("registerUserName").value = '';
+    document.getElementById("registerFirstName").value = '';
+    document.getElementById("registerLastName").value = '';
+    document.getElementById("registerPhoneNumber").value = '';
+    document.getElementById("registerPassword").value = '';
+}
