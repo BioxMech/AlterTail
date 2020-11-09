bb839f8423b89ecbfa2058cfd49f47b7de991fb9
Jason <70564653+BioxMech@users.noreply.github.com>
2020-11-09 18:20:54 +0800

    ...

--- database/load.sql
+++ database/load.sql
@@ -21,10 +21,12 @@ create table seller
 (email varchar(40) not null,
  shop_name varchar(30) not null,
  street_address varchar(50) not null,
+ shop_description varchar(200) not null,
  unit varchar(20),
  postal_code varchar(6) not null,
  shop_category varchar(20) not null,
+ rating varchar(10) not null,
+ rating_num varchar(10) not null,
  constraint seller_pk primary key(email),
  constraint seller_fk1 foreign key(email) references user(email)
 );
-
--- js/form.js
+++ js/form.js
@@ -17,9 +17,47 @@ function clearAllRegister() {
 function signInVerification() {
   var emailOutput = document.getElementById("emailOutput");
   var passwordOutput = document.getElementById("passwordOutput");
+
+  var request = new XMLHttpRequest(); // Prep to make an HTTP request
+    request.onreadystatechange = function() {
+
+        // Check if response is ready!
+        if( this.readyState == 4 && this.status == 200 ) {
+            
+            // Convert responseText to JSON
+            // var response_json = JSON.parse(this.responseText);
+            // var records = response_json.records;
+            
+            // so form can submit
+            return true
+        }
+        // Password wrong
+        else if (this.readyState == 4 && this.status == 401) {
+
+            passwordOutput.innerHTML = "Incorrect password.";
+
+            // so form will NOT submit
+            return false
+        }
+        // both wrong
+        else {
+
+            emailOutput.innerHTML = "Username does not exist.";
+
+            // so form will NOT submit
+            return false
+        }
+
+    }
+
+    // Using the api verification file to check if the email and password are correct
+    var url = "../projectAPI/user/verification.php?email=" + email + "&pw=" + password;
+
+    request.open("GET", url, true);
+
+    request.send();
+    
 }
-
-
 
 
 // Under register password, to make show/hide + eye logo dynamic
