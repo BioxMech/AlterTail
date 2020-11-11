5f135ea2c53c230f9e92551acc4588dffc97dc84
alina <alinaatxn@gmail.com>
2020-11-12 03:08:07 +0800

    ................................................

--- js/profile.js
+++ js/profile.js
@@ -129,9 +129,73 @@ function show_password()
 }
 
 console.log(sessionStorage.getItem("email"))
+document.getElementById("email").innerHTML = sessionStorage.getItem("email");
 
 function protect() {
     if (sessionStorage.length == 0 ) {
         window.location.replace("/AlterTail/index.html");
     }
 }
+
+function getProfileDetails(email, isProfilePage=true) {
+
+    var request = new XMLHttpRequest(); // Prep to make an HTTP request
+
+    request.onreadystatechange = function() {
+
+        // Check if response is ready!
+        if( this.readyState == 4 && this.status == 200 ) {
+            
+            // Convert responseText to JSON
+            var response_json = JSON.parse(this.responseText);
+            var records = response_json.records;
+
+            console.log(response_json)
+
+           for (profile of records) {
+            var email = profile.email;
+            var name = profile.fname;
+            var gender = profile.gender;
+            var username = profile.username;
+            var password = profile.pw;
+            var phone = profile.phone;
+            var address = profile.street_address;
+            var unit = profile.unit;
+            var postalCode = profile.postalCode;
+            // var image_url = profile.image_url;
+           }
+
+            if (isProfilePage) {
+
+            }
+            else {
+                var registerProfile = document.getElementById("content");
+        
+                registerProfile.innerHTML = `
+                <div class="dropdown">
+                    <a class="nav-link" href="profile.html" style="padding-bottom:unset;" 
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> ${username}&nbsp<img id="profileImage" src="images/profile_image.jpg" /></a>
+
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="profile.html">My Profile</a>
+                        <a class="dropdown-item" href="#" onclick="signOut()">Sign Out</a>
+                    </div>
+                </div>
+                `;
+            }
+
+            document.getElementById("content").innerHTML = registerProfile;
+
+        }
+
+    }
+
+    // Using the api to retrieve the user's profile details
+    var url = "projectAPI/user/retrieveProfile.php?email=" + email;
+
+    request.open("GET", url, true);
+
+    request.send();
+
+}
+
