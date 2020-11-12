a6eb499fe8c259eb3a02e8ed9050362875a3b4fc
alina <alinaatxn@gmail.com>
2020-11-12 23:12:49 +0800

    updated profile.html

--- js/profile.js
+++ js/profile.js
@@ -163,7 +163,9 @@ function getProfile(isProfilePage=true) {
             var postalCode = profile.postalCode;
             var image_url = profile.image_url;
            }
-           console.log(gender)
+
+           document.getElementById("name").innerHTML = name;
+           console.log(name);
 
             if (isProfilePage) {
                 var str = ""
@@ -181,25 +183,25 @@ function getProfile(isProfilePage=true) {
                                     <div class="form-group row">
                                     <label for="username" class="col-sm-3 col-form-label" style = "font-size: 15px; color: darkslategray; max-width: 120px;">Username</label>
                                     <div class="col-sm-9">
-                                        <input type="text" class="form-control" id="username" placeholder="${name}">
+                                        <input type="text" class="form-control" id="username" placeholder="" value =${name}>
                                     </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="email" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray;">Address</label>
+                                        <label for="email" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray; margin-right: 37px;">Address</label>
                                         <div class="col-sm-9">
-                                            <button type="button" class="btn btn-link" style = "color: darkslategray;">${address}</button>
+                                        <input type="text" class="form-control" id="address" placeholder="" value = ${address}>
                                         </div>
                                     </div>
                                     <div class="form-group row">
-                                        <label for="email" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray;">Email</label>
+                                        <label for="email" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray; margin-right: 37px;">Email</label>
                                         <div class="col-sm-9">
-                                            <button type="button" class="btn btn-link" style = "color: darkslategray;">${email}</button>
+                                        <input type="text" class="form-control" id="email" placeholder="" value = ${email}>
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <label for="num" class="col-sm-3 col-form-label" style = "font-size: 15px; color: darkslategray; padding-right: 0px; max-width: 120px;">Phone Number</label>
                                         <div class="col-sm-9">
-                                        <input type="password" class="form-control" id="num" placeholder="${phone}">
+                                        <input type="text" class="form-control" id="num" placeholder="" value = ${phone}>
                                         </div>
                                     </div>`;
                                     if(gender == 'Female')
@@ -321,4 +323,194 @@ function getProfile(isProfilePage=true) {
     request.send();
 
 }
+
+
+function getShop(isProfilePage=true) {
+
+    document.getElementById("content").innerHTML = "";
+    var request = new XMLHttpRequest(); // Prep to make an HTTP request
+
+    request.onreadystatechange = function() {
+
+        // Check if response is ready!
+        if( this.readyState == 4 && this.status == 200 ) {
+            
+            // Convert responseText to JSON
+            var response_json = JSON.parse(this.responseText);
+            var records = response_json.records;
+
+            console.log(response_json)
+
+           for (shop of records) {
+            var email = shop.email;
+            var shop_name = shop.shop_name;
+            var street_address = shop.street_address;
+            var shop_summary = shop.shop_summary;
+            var shop_description = shop.shop_description;
+            var unit = shop.unit;
+            var postalCode = shop.postalCode;
+            var shop_category = shop.shop_category;
+            var rating = shop.rating;
+            var rating_num = shop.rating_num;
+            var image_url = shop.image_url;
+           }
+
+
+             if (isProfilePage) {
+                 var str = ""
+                 var str1 =""
+                 str += `
+
+                       <div class = "col-lg-9 shadow bg-white rounded" style="padding: 30px">
+                        <div>
+                            <p style = "margin-bottom: 5px;">My Shop</p>
+                            <p style = "font-size: 15px; color: darkslategray;">Manage your shop</p>
+                        </div>
+                        <div class = "row">
+                            <div class = "col-lg-7">
+                                <form>
+                                    <div class="form-group row">
+                                    <label for="username" class="col-sm-3 col-form-label" style = "font-size: 15px; color: darkslategray; max-width: 120px;">Username</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" id="username" placeholder="${shop_name}">
+                                    </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="email" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray;">Address</label>
+                                        <div class="col-sm-9">
+                                            <button type="button" class="btn btn-link" style = "color: darkslategray;">${street_address}</button>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="email" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray;">Email</label>
+                                        <div class="col-sm-9">
+                                            <button type="button" class="btn btn-link" style = "color: darkslategray;">${email}</button>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="num" class="col-sm-3 col-form-label" style = "font-size: 15px; color: darkslategray; padding-right: 0px; max-width: 120px;">Phone Number</label>
+                                        <div class="col-sm-9">
+                                        <input type="password" class="form-control" id="num" placeholder="${shop_description}">
+                                        </div>
+                                    </div>`;
+                                    if(shop_category == 'Alteration')
+                                    {
+                                                            str1 += `
+                                                            <div class="form-group row">
+                                                                <label for="gender" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray;">Gender</label>
+                                                                <div class="form-check form-check-inline" style="margin-left: 15px;">
+                                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="male" value="male">
+                                                                    <label class="form-check-label" for="male" style = "font-size: 15px; color: darkslategray;">Male</label>
+                                                                </div>
+                                                                <div class="form-check form-check-inline">
+                                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="female" value="female" checked = "check">
+                                                                    <label class="form-check-label" for="female" style = "font-size: 15px; color: darkslategray;">Female</label>
+                                                                </div>
+                                                                <div class="form-check form-check-inline">
+                                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="other" value="other">
+                                                                    <label class="form-check-label" for="other" style = "font-size: 15px; color: darkslategray;">Other</label>
+                                                                </div>
+                                                            </div>   
+                                                            <div>
+                                                                <button type="button" class="btn btn-outline-secondary" style = "width: 70px; height: 40px; font-size: 15px;">Save</button>
+                                                            </div>                 
+                                                        </form>
+                                                    </div>
+                                                        
+                                                    <div class="col-lg-4" style="margin-top: 20px; margin-left: 50px;">
+                                                        <div class="card" style="width: 18rem; border: transparent; text-align: center;">
+                                                            <div class="img-square-wrapper">
+                                                                <img  id = "img1" class="" src="images/profile_image.jpg" alt="Card image cap">
+                                                            </div>
+                                                            <div class="card-body">
+                                                                <button type="button" class="btn btn-outline-secondary" style = "width: 120px; height: 35px; font-size: 15px;">Select Image</button>
+                                                                <p class="card-text" style = "font-size: 15px; color:gray; margin-top: 20px;">File size: maximum 1 MB</p>
+                                                                <p class="card-text" style = "font-size: 15px; color: gray; margin-left: 12px;">File extension: JPEG, PNG</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                        
+                                        `;
+
+                                    }
+
+                                    else
+                                    {
+                                                            str1 += `
+                                                            <div class="form-group row">
+                                                                <label for="gender" class="col-sm-2 col-form-label" style = "font-size: 15px; color: darkslategray;">Gender</label>
+                                                                <div class="form-check form-check-inline" style="margin-left: 15px;">
+                                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="male" value="male" checked = "check">
+                                                                    <label class="form-check-label" for="male" style = "font-size: 15px; color: darkslategray;">Male</label>
+                                                                </div>
+                                                                <div class="form-check form-check-inline">
+                                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="female" value="female">
+                                                                    <label class="form-check-label" for="female" style = "font-size: 15px; color: darkslategray;">Female</label>
+                                                                </div>
+                                                                <div class="form-check form-check-inline">
+                                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="other" value="other">
+                                                                    <label class="form-check-label" for="other" style = "font-size: 15px; color: darkslategray;">Other</label>
+                                                                </div>
+                                                            </div>   
+                                                            <div>
+                                                                <button type="button" class="btn btn-outline-secondary" style = "width: 70px; height: 40px; font-size: 15px;">Save</button>
+                                                            </div>                 
+                                                        </form>
+                                                    </div>
+                                                        
+                                                    <div class="col-lg-4" style="margin-top: 20px; margin-left: 50px;">
+                                                        <div class="card" style="width: 18rem; border: transparent; text-align: center;">
+                                                            <div class="img-square-wrapper">
+                                                                <img  id = "img1" class="" src="images/profile_image.jpg" alt="Card image cap">
+                                                            </div>
+                                                            <div class="card-body">
+                                                                <button type="button" class="btn btn-outline-secondary" style = "width: 120px; height: 35px; font-size: 15px;">Select Image</button>
+                                                                <p class="card-text" style = "font-size: 15px; color:gray; margin-top: 20px;">File size: maximum 1 MB</p>
+                                                                <p class="card-text" style = "font-size: 15px; color: gray; margin-left: 12px;">File extension: JPEG, PNG</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                        
+                                        `;
+                                    }
+                                    
+                                   
+                document.getElementById("content").innerHTML = str + str1;
+
+            }
+            // else {
+            //     var registerProfile = document.getElementById("try");
+        
+            //     registerProfile.innerHTML = `
+            //     <div class="dropdown">
+            //         <a class="nav-link" href="profile.html" style="padding-bottom:unset;" 
+            //         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> ${username}&nbsp<img id="profileImage" src="images/profile_image.jpg" /></a>
+
+            //         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            //             <a class="dropdown-item" href="profile.html">My Profile</a>
+            //             <a class="dropdown-item" href="#" onclick="signOut()">Sign Out</a>
+            //         </div>
+            //     </div>
+            //     `;
+            // }
+
+            // document.getElementById("try").innerHTML = registerProfile;
+
+        }
+
+    }
+
+    // Using the api to retrieve the user's profile details
+    var url = "projectAPI/user/retrieveShop.php?email=" + email;
+
+    request.open("GET", url, true);
+
+    request.send();
+
+}
+
 
---
+++ projectAPI/user/retrieveShop.php
@@ -0,0 +1,67 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/User.php';
+
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+//initialize object
+$user = new User($db);
+
+//query products
+$stmt = $user->retrieveShopPage($_GET["email"]);
+$num = $stmt->rowCount();
+
+if($num>0) {
+    $result_arr = array();
+    $result_arr["records"] = array();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $item = array(
+            "shop_name" => $shop_name,
+            "street_address" => $street_address,
+            "shop_summary" => $shop_summary,
+            "shop_description" => $shop_description,
+            "unit" => $unit
+            "postal_code" => $postal_code
+            "shop_category" => $shop_category
+            "rating" => $rating
+            "rating_num" => $rating_num
+            "img_url" => $img_url
+        );
+        array_push($result_arr["records"], $item);
+    }
+    $date = new DateTime(null, new DateTimeZone('Asia/Singapore'));
+    $result_arr["info"] = array(
+        "author" => "Jason",
+        "response_datetime_singapore" => $date->format('Y-m-d H:i:sP')
+    );
+
+    // set response code - 200 OK
+    http_response_code(200);
+
+    // show products data in json format
+    echo json_encode($result_arr);
+}
+else {
+    // set response code - 404 Not found
+    http_response_code(404);
+  
+    // tell the user no items found
+    echo json_encode(
+        array("message" => "No records found.")
+    );
+}
+
+
+
+
+?>
