a93f0cbb50106a4b0a7c4f8fa7411d80de4d197e
Jason <70564653+BioxMech@users.noreply.github.com>
2020-11-14 14:16:22 +0800

    A little update to the Search Bar

--- js/SuperSaaS.js
+++ js/SuperSaaS.js
@@ -48,7 +48,7 @@ function getAvailabilities(schedule_id) {
       html_str += `<li class='dropdown-item' value='${booking_start}'>${date} ${start_time}-${end_time}</li>`;
 
     }
-    console.log(html_str);
+    // console.log(html_str);
     document.getElementById("dropdownMenu").innerHTML = html_str;
   })
   .catch((err) => {
